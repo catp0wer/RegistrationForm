72fe84b4ad3e891a0e3b4f59764683e031cecbc7
catp0wer <catp0wer@mail.ru>
2018-04-16 13:23:25 +0300

    remove the method for each element instead add the variables in which the element is stored

--- src/main/java/Pom/RegistrationForm.java
+++ src/main/java/Pom/RegistrationForm.java
@@ -5,64 +5,43 @@ import org.openqa.selenium.WebElement;
 
 public class RegistrationForm {
 
-    public WebDriver driver;
 
-    public static WebElement firstName(WebDriver driver) {
-        return driver.findElement(By.id("name_3_firstname"));
+    By firstName = By.id("name_3_firstname");
+    By lastName = By.id("name_3_lastname");
+    By singleMaritalStatus = By.xpath("//input[@value='single']");
+    By marriedMaritalStatus = By.xpath("//input[@value='married']");
+    By divorcedMaritalStatus = By.xpath("//input[@value='divorced']");
+    By danceHobby = By.xpath("//input[@value='dance']");
+    By readingHobby = By.xpath("//input[@value='dance']");
+    By cricketHobby = By.xpath("//input[@value='cricket']");
+    By countryDropDown = By.id("dropdown_7");
+    By monthBirthday =By.id("mm_date_8");
+    By dayBirthday = By.id("dd_date_8");
+    By yearBirthday = By.id("yy_date_8");
+    By phoneNumber = By.id("phone_9");
+    By userName = By.id("username");
+    By email = By.id("email_1");
+    By aboutYourself = By.id("description");
+    By password = By.id("password_2");
+    By confirmPassword = By.id("confirm_password_password_2");
+    By Submit = By.name("pie_submit");
+
+    public void fillFirstName (String name, WebDriver driver){
+        driver.findElement(firstName).sendKeys(name);
     }
-    public static WebElement lastName(WebDriver driver) {
-        return driver.findElement(By.id("name_3_lastname"));
+    public void fillLastName (String name, WebDriver driver){
+        driver.findElement(lastName).sendKeys(name);
     }
-    public static WebElement singleMaritalStatus(WebDriver driver){
-        return driver.findElement(By.xpath("//input[@value='single']"));
-    }
-    public static WebElement marriedMaritalStatus(WebDriver driver){
-        return driver.findElement(By.xpath("//input[@value='married']"));
-    }
-    public static WebElement divorcedMaritalStatus(WebDriver driver){
-        return driver.findElement(By.xpath("//input[@value='divorced']"));
-    }
-    public static WebElement danceHobby(WebDriver driver){
-        return driver.findElement(By.xpath("//input[@value='dance']"));
-    }
-    public static WebElement readingHobby(WebDriver driver){
-        return driver.findElement(By.xpath("//input[@value='dance']"));
-    }
-    public static WebElement cricketHobby(WebDriver driver){
-        return driver.findElement(By.xpath("//input[@value='cricket']"));
-    }
-    public static WebElement countryDropDown(WebDriver driver){
-        return driver.findElement(By.id("dropdown_7']"));
+    public void clickSubmit(WebDriver driver){
+        driver.findElement(Submit).click();
     }
 
-    public static WebElement monthBirthday(WebDriver driver){
-        return driver.findElement(By.id("mm_date_8']"));
+    public void login(String name,String lastName,WebDriver driver) {
+        fillFirstName(name, driver);
+        fillLastName(lastName, driver);
+        clickSubmit(driver);
+        //   assert driver.getPageSource().contains(SubmitMessage);
     }
-    public static WebElement dayBirthday(WebDriver driver){
-        return driver.findElement(By.id("dd_date_8']"));
-    }
-    public static WebElement yearBirthday(WebDriver driver){
-        return driver.findElement(By.id("yy_date_8']"));
-    }
-    public static WebElement phoneNumber(WebDriver driver){
-        return driver.findElement(By.id("phone_9']"));
-    }
-    public static WebElement userName(WebDriver driver){
-        return driver.findElement(By.id("username']"));
-    }
-    public static WebElement email(WebDriver driver){
-        return driver.findElement(By.id("email_1']"));
-    }
-    public static WebElement aboutYourself(WebDriver driver){
-        return driver.findElement(By.id("description']"));
-    }
-    public static WebElement password(WebDriver driver){
-        return driver.findElement(By.id("password_2']"));
-    }
-    public static WebElement confirmPassword(WebDriver driver){
-        return driver.findElement(By.id("confirm_password_password_2']"));
-    }
-    public static WebElement Submit(WebDriver driver){
-        return driver.findElement(By.name("pie_submit']"));
-    }
+
+
 }
