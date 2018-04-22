242f538d70e916bff75abb42ddaf435e787f5c9c
catp0wer <catp0wer@mail.ru>
2018-04-22 16:57:07 +0300

    added data for all fields

--- src/main/java/DataProvider_Repository.java
+++ src/main/java/DataProvider_Repository.java
@@ -9,12 +9,15 @@ public class DataProvider_Repository {
     public static Object[][] LogInCaseData(){
         return new Object[][]
                 {
-                        { "Test1","" },
-                        { "Test2", "Test" },
-                        { "Test3", "Test2" }
+                        //firtsname, lastName,maritalStatus,Hobby, Country,Month,day,Year,Phone number, username,
+                        //Email,About Yourself, pass, Confirm pass
+                        { "Testaut3","Testaut3","","Reading","Brunei","3","2","2005" ,"1234567899","testaut3",
+                               "testaut3@gmail.com","I am test1","test1test1","test1test1","Thank you for your registration"},
+                      //  { "Testaut100", "Testaut100","","Dance","Moldova","3","5","2005","1111111111","testaut100",
+                      //      "testaut100@gmail.com","","test2test2","test2test2","Thank you for your registration"},
+                      //  { "Test3", "Test3","Single","Cricket","Moldova","1","1","2000","2222222222","test3",
+                        // "test3@gmail.com","I am test3","test3","test3"}
 
                 };
-
-
     }
 }
